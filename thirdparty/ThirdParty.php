c54c32cc25e920e94ef97cd33f779713c3f50130
fstcore <danupermanaia@gmail.com>
2025-06-24 01:44:59 -0700

    1.0

--- thirdparty/ThirdParty.php
+++ thirdparty/ThirdParty.php
@@ -1,9 +1,6 @@
 <?php
 
 $basedir = preg_replace('/\\\lib.*|\/lib.*/', '', __DIR__);
-if(file_exists($basedir.'/lib/thirdparty/vendor/autoload.php')){
-    exit;
-}
 require_once($basedir.'/lib/thirdparty/vendor/autoload.php');
 
 ?>
